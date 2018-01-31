e079a79463fd6bf5d956c4c1f1ee39da3b5e20d1
Andrius Degutis <andriusdgt@gmail.com>
2018-01-31 16:58:25 +0200

    sets previous block hashes

--- src/main/kotlin/lt/okaminu/blockchain/Block.kt
+++ src/main/kotlin/lt/okaminu/blockchain/Block.kt
@@ -2,5 +2,5 @@ package lt.okaminu.blockchain
 
 import java.time.Instant
 
-data class Block (val data: String, val timestamp: Long = Instant.now().epochSecond,
-                  var hash: String = "", var previousHash: String = "", var nounce: Int = 0)
+data class Block (val data: String, var previousHash: String, var hash: String = "",
+                  val timestamp: Long = Instant.now().epochSecond, var nounce: Int = 0)
