62f0aba4390d32f5388048355084405d6bbe5288
Andrius Degutis <andriusdgt@gmail.com>
2018-01-31 16:31:42 +0200

    added genesis block to blockchain

--- src/main/kotlin/lt/okaminu/blockchain/BlockChain.kt
+++ src/main/kotlin/lt/okaminu/blockchain/BlockChain.kt
@@ -1,9 +1,10 @@
 package lt.okaminu.blockchain
 
-class BlockChain(private val blocks: ArrayList<Block> = arrayListOf()) {
+class BlockChain(private val blocks: ArrayList<Block> = arrayListOf(Block("genesis block"))) {
 
     fun addBlock(data: String) = blocks.add(Block(data))
 
-    fun printAllBlocks() = blocks.forEach { println("time=${it.timestamp} data=${it.data} nounce=${it.nounce}") }
+    fun printAllBlocks() =
+            blocks.forEach { println("time=${it.timestamp} data=${it.data} nounce=${it.nounce} hash=${it.hash}") }
 
 }
