b4481439cfebcc3343d59c1a1d15067a3faaf4c1
Andrius Degutis <andriusdgt@gmail.com>
2018-01-31 16:26:10 +0200

    nonce added

--- src/main/kotlin/lt/okaminu/blockchain/BlockChain.kt
+++ src/main/kotlin/lt/okaminu/blockchain/BlockChain.kt
@@ -1,11 +1,9 @@
 package lt.okaminu.blockchain
 
-import java.time.Instant.now
-
 class BlockChain(private val blocks: ArrayList<Block> = arrayListOf()) {
 
-    fun addBlock(data: String) = blocks.add(Block(now().epochSecond, data))
+    fun addBlock(data: String) = blocks.add(Block(data))
 
-    fun printAllBlocks() = blocks.forEach { println("time=${it.timestamp} data=${it.data}") }
+    fun printAllBlocks() = blocks.forEach { println("time=${it.timestamp} data=${it.data} nounce=${it.nounce}") }
 
 }
