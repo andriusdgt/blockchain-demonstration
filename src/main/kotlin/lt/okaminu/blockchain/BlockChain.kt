79208f6e34702d9bff7640cf05f20ef58a9abe9f
Andrius Degutis <andriusdgt@gmail.com>
2018-01-31 16:19:21 +0200

    block contains timestamp

--- src/main/kotlin/lt/okaminu/blockchain/BlockChain.kt
+++ src/main/kotlin/lt/okaminu/blockchain/BlockChain.kt
@@ -1,6 +1,11 @@
 package lt.okaminu.blockchain
 
-class BlockChain (private val blocks: ArrayList<Block> = arrayListOf()){
-    fun addBlock(data: String) = blocks.add(Block(data))
-    fun printAllBlocks() = blocks.forEach{println(it.data)}
+import java.time.Instant.now
+
+class BlockChain(private val blocks: ArrayList<Block> = arrayListOf()) {
+
+    fun addBlock(data: String) = blocks.add(Block(now().epochSecond, data))
+
+    fun printAllBlocks() = blocks.forEach { println("time=${it.timestamp} data=${it.data}") }
+
 }
