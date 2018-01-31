4b53a5cbccb4ad2eb1be5423693e6bc458e41d70
Andrius Degutis <andriusdgt@gmail.com>
2018-01-31 16:58:25 +0200

    sets previous block hashes

--- src/main/kotlin/lt/okaminu/blockchain/BlockChain.kt
+++ src/main/kotlin/lt/okaminu/blockchain/BlockChain.kt
@@ -1,16 +1,14 @@
 package lt.okaminu.blockchain
 
-class BlockChain(private val blocks: ArrayList<Block> = arrayListOf(Block("genesis block"))) {
+class BlockChain(private val blocks: ArrayList<Block> = arrayListOf(Block("genesis block", "123"))) {
 
     fun addBlock(data: String){
-        val block = Block(data)
+        val block = Block(data, blocks.last().previousHash)
         val (hash, nounce) = calculateHash(block)
         block.hash = hash
         block.nounce = nounce
         blocks.add(block)
-
     }
-
 
     fun printAllBlocks() =
             blocks.forEach { println("time=${it.timestamp} data=${it.data} nounce=${it.nounce} hash=${it.hash}") }
