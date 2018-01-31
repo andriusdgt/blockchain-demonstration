3acb281dee63c49c2919cf1fedfe569b4f8f8fd5
Andrius Degutis <andriusdgt@gmail.com>
2018-01-31 17:13:48 +0200

    calculating hash

--- src/main/kotlin/lt/okaminu/blockchain/HashCalculator.kt
+++ src/main/kotlin/lt/okaminu/blockchain/HashCalculator.kt
@@ -1,3 +1,8 @@
 package lt.okaminu.blockchain
 
-fun calculateHash(block: Block) = Pair(block.hash, block.nounce)
+fun calculateHash(block: Block): Pair<String, Int> {
+    block.nounce++
+    val str = "${block.data}${block.previousHash}${block.timestamp}${block.nounce}"
+
+    return Pair(str, block.nounce)
+}
